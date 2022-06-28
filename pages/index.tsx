2849238899ca4a182519d35748d4b04f067e9a75
Arpit Aggarwal <arpit@candorhealth.com>
2022-06-29 00:51:30 +0530

    formatting issues + added one emoji

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hello!
+            Hello! 👋
           </h1>
           <div className="text-lg leading-8 text-gray-600 dark:text-gray-400">
             <h1 className="text-neutral-900 dark:text-neutral-200">
